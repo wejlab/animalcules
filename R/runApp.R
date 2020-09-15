74cb1684e397c7e04093960c97b6c9dfbea61e52
Anthony Federico <anfederico@users.noreply.github.com>
2020-09-15 18:13:50 -0400

    Add dev option

--- R/runApp.R
+++ R/runApp.R
@@ -7,16 +7,21 @@
 #' @importFrom shinyjs addClass
 #' @return The shiny app will open
 #'
+#' @param dev Run the applicaiton in developer mode
+#' 
 #' @examples 
 #' \dontrun{
 #' run_animalcules()
 #' }
 #' @export
-run_animalcules <- function() {
+run_animalcules <- function(dev=FALSE) {
     appDir <- system.file("shiny", package = "animalcules")
     if (appDir == "") {
         stop("Could not find myapp. Try re-installing `mypackage`.", 
             call. = FALSE)
+    }
+    if (dev) {
+        options(shiny.autoreload=TRUE)
     }
     shiny::runApp(appDir, display.mode = "normal")
 }
