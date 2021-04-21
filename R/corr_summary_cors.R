8fc405344ae592598d50f5f79d6377e858aaa745
Saket  Pandit <saketpandit@Sakets-MacBook-Pro.local>
2021-04-21 16:29:00 -0400

    Functions called within corr_func that produce a heatmap and a summary table

---
+++ R/corr_summary_cors.R
@@ -0,0 +1,28 @@
+#' Summarize significant correlations
+#' 
+#' @param cormat A matrix of significant correlations
+#' 
+#' @return A summary table of the correlated groups, as well as how big each group is
+#' 
+#' @export
+
+summary_cors <- function(cormat){
+  ns <- c()
+  os <- c()
+  gs <- c()
+  # Get the names 
+  for(otu in rownames(cormat)){
+    grp <- names(which(cormat[otu,]>0))
+    num <- length(grp)
+    if(num > 0){
+      ns <- c(ns, num)
+      os <- c(os, otu)
+      grp <- paste0(grp, sep = ";", collapse = "")
+      gs <- c(gs, grp)
+    }
+  }
+  s <- data.frame(OTU = os,
+                  Group_Size = ns,
+                  Groups = gs)
+  return(s)
+}
