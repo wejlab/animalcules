fd10c1618877cfa7ed7998476bbb3f879f0c6263
jessmcc22Mac <jessmcc@bu.edu>
2023-03-31 14:42:26 -0400

    Updated the ROC plot using ROCit inctead of plotROC

--- R/find_biomarker.R
+++ R/find_biomarker.R
@@ -11,14 +11,6 @@
 #'
 #' @return A list
 #'
-#' @import MultiAssayExperiment
-#' @import glmnet
-#' @import caret
-#' @import plotROC
-#' @import forcats
-#' @importFrom tibble rownames_to_column
-#' @importFrom ggplot2 geom_col aes coord_flip theme_bw coord_equal annotate
-#'
 #' @examples
 #' data_dir <- system.file("extdata/MAE.rds", package = "animalcules")
 #' toy_data <- readRDS(data_dir)
@@ -45,147 +37,135 @@ find_biomarker <- function(MAE,
         "logistic regression",
         "random forest"
     )) {
-    . <- NULL
+    
     ## SEED bioC not suggesst add set seed function in R code set.seed(seed)
     ## tables from MAE
     microbe <- MAE[["MicrobeGenetics"]] # double bracket subsetting is easier
-    tax_table <- as.data.frame(rowData(microbe)) # organism x taxlev
-    sam_table <- as.data.frame(colData(microbe)) # sample x condition
-    counts_table <-
-        as.data.frame(assays(microbe))[, rownames(sam_table)] #organism x sample
+    tax_table <- SummarizedExperiment::rowData(microbe) |>
+        as.data.frame()
+    sam_table <- SummarizedExperiment::colData(microbe) |>
+        as.data.frame()
+    counts_table <- SummarizedExperiment::assays(microbe) |>
+        as.data.frame() |> 
+        dplyr::select(dplyr::all_of(rownames(sam_table)))
     ## shiny UI input object
     # Sum counts by taxon level and return log10 cpm
-    logcpm_table <- counts_table %>%
-        upsample_counts(tax_table, tax_level) %>%
-        counts_to_logcpm() %>%
-        base::t() %>%
-        base::as.data.frame()
+    logcpm_table <- counts_table |>
+        upsample_counts(tax_table, tax_level) |>
+        counts_to_logcpm() |> base::t() |> base::as.data.frame()
     # add target variable
     logcpm_table[, "y"] <-
-        sam_table %>% dplyr::pull(input_select_target_biomarker)
+        sam_table |> dplyr::pull(input_select_target_biomarker)
     # set up classification model prameters
     fitControl <- caret::trainControl(
         method = "repeatedcv",
         number = nfolds,
         repeats = nrepeats,
         classProbs = TRUE,
-        summaryFunction = twoClassSummary,
+        summaryFunction = caret::twoClassSummary,
         savePredictions = TRUE
     )
     # choose different model
     if (model_name == "logistic regression") {
         model_fit <-
-            caret::train(y ~ .,
-                data = logcpm_table, method = "glmnet",
-                tuneLength = 5, trControl = fitControl, metric = "ROC"
-            )
+            caret::train(y ~ ., data = logcpm_table, method = "glmnet",
+                tuneLength = 5, trControl = fitControl, metric = "ROC")
     } else if (model_name == "svm") {
-        model_fit <-
-            caret::train(y ~ .,
-                data = logcpm_table, method = "svmLinear",
-                tuneLength = 5, trControl = fitControl, metric = "ROC"
-            )
+        model_fit <- caret::train(y ~ ., data = logcpm_table,
+            method = "svmLinear", tuneLength = 5, 
+            trControl = fitControl, metric = "ROC")
     } else if (model_name == "gbm") {
-        model_fit <-
-            caret::train(y ~ .,
-                data = logcpm_table,
-                method = "gbm", trControl = fitControl,
-                tuneLength = 5, metric = "ROC", verbose = FALSE
-            )
+        model_fit <- caret::train(y ~ ., data = logcpm_table, method = "gbm",
+            trControl = fitControl, tuneLength = 5,
+            metric = "ROC", verbose = FALSE)
     } else if (model_name == "random forest") {
-        model_fit <-
-            caret::train(y ~ .,
-                data = logcpm_table, method = "ranger",
-                trControl = fitControl, tuneLength = 5,
-                metric = "ROC", importance = "impurity"
-            )
+        model_fit <- caret::train(y ~ ., data = logcpm_table, method = "ranger",
+            trControl = fitControl, tuneLength = 5,
+            metric = "ROC", importance = "impurity")
     }
     # process the importance score
     if (model_name == "svm") {
         svm_importance <- caret::varImp(model_fit)$importance
         svm_importance[, 2] <- NULL
         colnames(svm_importance) <- "importance"
-        biom_pre <- svm_importance %>%
-            rownames_to_column() %>%
-            dplyr::rename("biomarker" = "rowname") %>%
-        dplyr::arrange("importance")
+        biom_pre <- svm_importance |> tibble::rownames_to_column() |>
+            dplyr::rename("biomarker" = "rowname") |>
+            dplyr::arrange("importance")
         ind <- biom_pre$importance > quantile(biom_pre$importance, 
             1 - percent_top_biomarker)
-        biomarker <- biom_pre %>% 
-            dplyr::filter(ind) %>% 
-            dplyr::pull("biomarker")
-            
-        imp_plot_pre <- biom_pre %>% dplyr::filter(ind) %>%
+        biomarker <- biom_pre |>  dplyr::filter(ind) |>
+            dplyr::pull(biomarker)
+        imp_plot_pre <- biom_pre |> dplyr::filter(ind) |>
             dplyr::mutate(biomarker = forcats::fct_inorder(biomarker))
         importance_plot <- ggplot2::ggplot(imp_plot_pre) +
-            geom_col(aes(x = "biomarker", y = "importance")) +
-            coord_flip() +
-            theme_bw()
+            ggplot2::geom_col(ggplot2::aes_string(x = 'biomarker', y = 'importance')) +
+            ggplot2::coord_flip() +
+            ggplot2::theme_bw()
     } else {
-        biom_pre <- caret::varImp(model_fit)$importance %>%
-            base::as.data.frame() %>%
-            rownames_to_column() %>%
-            dplyr::rename("importance" = "Overall") %>%
-            dplyr::rename("biomarker" = "rowname") %>%
+        biom_pre <- caret::varImp(model_fit)$importance |>
+            base::as.data.frame() |>
+            tibble::rownames_to_column() |>
+            dplyr::rename("importance" = "Overall") |>
+            dplyr::rename("biomarker" = "rowname") |>
             dplyr::arrange("importance")
         ind <- biom_pre$importance >
-                    quantile(biom_pre$importance, 1 - percent_top_biomarker)
-        biomarker <- biom_pre %>% dplyr::filter(ind) %>%
-            dplyr::pull("biomarker") 
+            quantile(biom_pre$importance, 1 - percent_top_biomarker)
+        biomarker <- biom_pre |> dplyr::filter(ind) |>
+            dplyr::pull(biomarker) 
         
-         importance_plot <-  biom_pre %>% dplyr::filter(ind) %>%
-            dplyr::mutate("biomarker" = forcats::fct_inorder("biomarker")) %>%
+        importance_plot <-  biom_pre |> dplyr::filter(ind) |>
+            dplyr::mutate(biomarker = forcats::fct_inorder(biomarker)) |>
             ggplot2::ggplot() +
-            geom_col(aes(x = "biomarker", y = "importance")) +
-            coord_flip() +
-            theme_bw()
+            ggplot2::geom_col(ggplot2::aes_string(x = 'biomarker', y = 'importance')) +
+            ggplot2::coord_flip() +
+            ggplot2::theme_bw()
     }
     # retrain the model using the biomarker
-    logcpm_table <- logcpm_table %>% dplyr::select(biomarker, "y")
+    logcpm_table <- logcpm_table |>
+        dplyr::select(dplyr::all_of(biomarker), "y")
     # choose different model
     if (model_name == "logistic regression") {
-        model_fit <- caret::train(y ~ .,
-            data = logcpm_table, method = "glmnet",
-            tuneLength = 5, trControl = fitControl, metric = "ROC"
-        )
+        model_fit <- caret::train(
+            y ~ ., data = logcpm_table, method = "glmnet",
+            tuneLength = 5, trControl = fitControl, metric = "ROC")
     } else if (model_name == "svm") {
-        model_fit <- caret::train(y ~ .,
-            data = logcpm_table, method = "svmLinear",
-            tuneLength = 5, trControl = fitControl, metric = "ROC"
-        )
+        model_fit <- caret::train(
+            y ~ ., data = logcpm_table, method = "svmLinear",
+            tuneLength = 5, trControl = fitControl, metric = "ROC")
     } else if (model_name == "gbm") {
-        model_fit <- caret::train(y ~ .,
-            data = logcpm_table, method = "gbm", trControl = fitControl,
-            tuneLength = 5, metric = "ROC", verbose = FALSE
-        )
+        model_fit <- caret::train(
+            y ~ ., data = logcpm_table, method = "gbm",
+            trControl = fitControl, tuneLength = 5, metric = "ROC", verbose = FALSE)
     } else if (model_name == "random forest") {
-        model_fit <- caret::train(y ~ .,
-            data = logcpm_table, method = "ranger",
-            trControl = fitControl, tuneLength = 5,
-            metric = "ROC", importance = "impurity"
-        )
+        model_fit <- caret::train(
+            y ~ ., data = logcpm_table, method = "ranger", trControl = fitControl,
+            tuneLength = 5, metric = "ROC", importance = "impurity")
     }
-    prob_pred <- as.numeric(model_fit$pred$obs)
+    mod_pred <- model_fit$pred |> tibble::as_tibble()
+    prob_pred <- as.numeric(mod_pred$obs)
     prob_pred[prob_pred == 1] <- 0
     prob_pred[prob_pred == 2] <- 1
-    df_roc <- data.frame(
-        m = model_fit$pred[
-            ,
-            which(colnames(model_fit$pred) == levels(model_fit$pred$obs)[2])
-        ],
-        d = prob_pred, stringsAsFactors = FALSE
-    )
-    g <- ggplot(df_roc, aes(m = df_roc$m, d = df_roc$d)) +
-        geom_roc(n.cuts = 0) +
-        coord_equal() +
-        style_roc()
-    roc_plot <- g + annotate("text",
-        x = 0.75, y = 0.25,
-        label = paste("AUC =", round(
-            (calc_auc(g))$AUC,
-            4
-        ))
-    )
+    
+    neg_pos <- mod_pred |> dplyr::pull(dplyr::all_of(levels(model_fit$pred$obs)[2]))
+    df_roc <- tibble::tibble(m = neg_pos, d = prob_pred)
+    
+    this_rocit <- ROCit::rocit(df_roc$m, df_roc$d)
+    this_label <- paste("AUC =", round(this_rocit$AUC, 4))
+    
+    plot_dat <- tibble::tibble(FPR = this_rocit$FPR, TPR = this_rocit$TPR)
+    
+    roc_plot <- ggplot2::ggplot(data = plot_dat, ggplot2::aes(x = 'FPR', y = 'TPR')) +
+        ggplot2::geom_line(ggplot2::aes_string(x = 'FPR', y = 'TPR', col = base::shQuote("Empirical ROC curve"))) +
+        ggplot2::geom_abline(ggplot2::aes(intercept = 0, slope = 1, col = "Chance line"), size = 1,
+            linetype = "dashed", show.legend = FALSE, alpha = 0.5) +
+        ggplot2::labs(x = "1-Specificity (FPR)", y = "Sensitivity (TPR)",
+            title = "Emperical ROC Curve", subtitle = this_label) +
+        ggplot2::scale_color_manual("",
+            labels = c("Chance line", "Empirical ROC curve"),
+            values = c("grey", "red")) +
+        ggplot2::theme_classic() +
+        ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1))
+    
     biomarker <- data.frame(biomarker_list = biomarker)
     # output a list
     list_output <- list(
